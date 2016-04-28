946a6ca46a788cc65c46ff7cca4ff76610681ead
doloreel <jarimado@gmail.com>
2016-04-29 09:02:18 +1000

    other

---
+++ 01_javascript/functions/main.js
@@ -0,0 +1,9 @@
+console.log('sup');
+
+var times3nums = function (n1, n2, n3) {
+    var result = n1 * n2 * n3;
+    var message = "The result was: " + result;
+    return message;
+}
+
+times3nums(1, 2, 3); 
---
+++ 01_javascript/loops/main.js
@@ -0,0 +1,42 @@
+console.log('loops')
+
+// Exercises: For loops
+// The even/odd reporter
+//
+// Write a for loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd, and report that to the screen (e.g. "2 is even").
+//
+
+for (var i = 0; i < 21; i += 1) {
+    if (i % 2 === 0) {
+        console.log(i, ' is even')
+    } else {
+        console.log(i, ' is odd');
+    };
+}
+
+// Multiplication Tables
+//
+// Write a for loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
+//
+// Bonus: Use a nested for loop to show the tables for every multiplier from 1 to 10 (100 results total).
+//
+
+// The Grade Assigner
+//
+// Check the results of assignGrade function from the conditionals exercise for every value from 60 to 100 - so your log should show "For 89, you got a B. For 90, you got an A.", etc.
+
+
+for (var i = 60; i < 101; i += 1) {
+    if ( i < 101 && i >= 90 ) {
+        console.log ('For ' + i + ', you got a A');
+    }
+    if ( i < 90 && i >= 80 ) {
+        console.log ('For ' + i + ', you got a B');
+    }
+    if ( i < 80 && i >= 70 ) {
+        console.log ('For ' + i + ', you got a C');
+    }
+    if ( i <  70 ) {
+        console.log ('For ' + i + ', you got a D');
+    }
+}
